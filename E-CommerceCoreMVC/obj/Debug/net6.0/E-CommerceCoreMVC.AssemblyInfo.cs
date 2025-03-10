87f932490a5e7e89e00a9f70c7401cd6233e4112
NQDungx9 <dnguen82@gmail.com>
2025-03-10 23:00:03 +0700

    Add InventoryTransaction

--- E-CommerceCoreMVC/obj/Debug/net6.0/E-CommerceCoreMVC.AssemblyInfo.cs
+++ E-CommerceCoreMVC/obj/Debug/net6.0/E-CommerceCoreMVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-CommerceCoreMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d66bf7a58a3e977b2446e8bc35588fd55d99fd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-CommerceCoreMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-CommerceCoreMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
